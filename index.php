cbbd74cf3573a89d06fc042401bebfb64d2a5938
Tyler Cypher <tcyph33@gmail.com>
2018-04-24 14:48:54 -0400

    set up index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Includes Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <link rel="stylesheet" href="css/stylesheet.css" />
+
+</head>
+    <body>
+
+        <div id="container">
+
+        <?php
+
+            include ('includes/header.html');
+            include ('includes/nav.html');
+            include ('includes/content.html');
+        ?>
+
+        </div>
+
+    </body>
+</html>
