9dfa33027cff63df7f29b75bc2c77ea85e2fc0f3
shanetorres <stor314@gmail.com>
2018-05-01 10:33:55 -0400

    add div new story on click

--- index.php
+++ index.php
@@ -8,9 +8,10 @@
     <meta charset="UTF-8">
     <meta name="description" content="">
     <meta name="keywords" content="">
-
+    
     <link rel="stylesheet" type="text/css" href="css/stylesheet.css?version=1" media="screen" />
     <link title='Roboto' href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <!-- UIkit CSS -->
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.42/css/uikit.min.css" /> -->
 
