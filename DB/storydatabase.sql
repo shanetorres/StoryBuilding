f7a67f71e52e32ee61fd955f066a78f60564e0a0
Tyler Cypher <tcyph33@gmail.com>
2018-05-08 15:16:06 -0400

    updated db

--- DB/storydatabase.sql
+++ DB/storydatabase.sql
@@ -6,4 +6,5 @@ create table Story(
 storyID int not null AUTO_INCREMENT,
 storyText varchar(2000),
 storyAuthor varchar(30),
+storyTitle varchar(30),
 constraint pk_storyID primary key (storyID)) ENGINE = INNODB;
